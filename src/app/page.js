0099c400b1c48dbabfa94164e60797b305401987
Dibyajyoti Sahoo <dibyajyotisahoo2504@gmail.com>
2024-04-18 21:04:13 +0530

    Add clickable button to show dialog box

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,21 @@
+"use client";
+
+import { useState } from "react";
+
 export default function Home() {
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="flex place-items-center mt-48">
-        <button className="text-xl bg-green-700 p-3 rounded-lg font-mono">
+        <button
+          className="text-xl bg-green-700 p-3 rounded-lg font-mono"
+          onClick={handleClickOpen}
+        >
           Generate Reports
         </button>
       </div>
