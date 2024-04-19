fd925292f79b0e8582293395dc1e576e5ebbd069
Dibyajyoti Sahoo <dibyajyotisahoo2504@gmail.com>
2024-04-19 15:02:18 +0530

    Create a pagination component

--- src/app/components/paginate/page.js
+++ src/app/components/paginate/page.js
@@ -1,3 +1,9 @@
-export default function Paginate() {
-  return <div>Paginate</div>;
+export default function Pagination() {
+  return (
+    <div>
+      <h1 className="text-center text-gray-800 font-medium leading-6">
+        Pagination
+      </h1>
+    </div>
+  );
 }
