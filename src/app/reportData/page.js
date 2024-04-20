3c4c8d3caaeda930560e323e38fd91b83c79e3e3
Dibyajyoti Sahoo <dibyajyotisahoo2504@gmail.com>
2024-04-20 12:23:52 +0530

    Create a separate report data file

---
+++ src/app/reportData/page.js
@@ -0,0 +1,159 @@
+const reportData = [
+  {
+    date: "22.07.2021",
+    reportName: "APP_URL_USAGE_(Exclude_Half_Days)_3_6_2021_3_31_2021.csv",
+    downloadLink: "#",
+  },
+  {
+    date: "20.07.2021",
+    reportName: "RUR_L1_01_01_2021_31_03_2021.csv",
+    downloadLink: "#",
+  },
+  {
+    date: "19.07.2021",
+    reportName: "RUR_L2_(Exclude_NonWorking_Days)_4_1_2021_4_1_2021.csv",
+    downloadLink: "#",
+  },
+  {
+    date: "18.07.2021",
+    reportName: "RUR_L3_05_01_2021_05_31_2021.csv",
+    downloadLink: "#",
+  },
+  {
+    date: "17.07.2021",
+    reportName: "APP_ACTIVITY_06_2021.csv",
+    downloadLink: "#",
+  },
+  {
+    date: "16.07.2021",
+    reportName: "USER_STATS_202106.csv",
+    downloadLink: "#",
+  },
+  {
+    date: "15.07.2021",
+    reportName: "SALES_REPORT_2021_JUNE.csv",
+    downloadLink: "#",
+  },
+  {
+    date: "14.07.2021",
+    reportName: "RUR_L1_2021_JUNE.csv",
+    downloadLink: "#",
+  },
+  {
+    date: "13.07.2021",
+    reportName: "RUR_L2_202106.csv",
+    downloadLink: "#",
+  },
+  {
+    date: "12.07.2021",
+    reportName: "APP_USAGE_JUNE_2021.csv",
+    downloadLink: "#",
+  },
+  {
+    date: "11.07.2021",
+    reportName: "USER_ACTIVITY_06_2021.csv",
+    downloadLink: "#",
+  },
+  {
+    date: "10.07.2021",
+    reportName: "RUR_L3_06_2021.csv",
+    downloadLink: "#",
+  },
+  {
+    date: "09.07.2021",
+    reportName: "SALES_REPORT_JUNE_2021.csv",
+    downloadLink: "#",
+  },
+  {
+    date: "08.07.2021",
+    reportName: "RUR_L1_JUNE_2021.csv",
+    downloadLink: "#",
+  },
+  {
+    date: "07.07.2021",
+    reportName: "RUR_L2_JUNE_2021.csv",
+    downloadLink: "#",
+  },
+  {
+    date: "06.07.2021",
+    reportName: "APP_USAGE_06_2021.csv",
+    downloadLink: "#",
+  },
+  {
+    date: "05.07.2021",
+    reportName: "USER_STATS_JUNE_2021.csv",
+    downloadLink: "#",
+  },
+  {
+    date: "04.07.2021",
+    reportName: "SALES_REPORT_2021_JUNE.csv",
+    downloadLink: "#",
+  },
+  {
+    date: "03.07.2021",
+    reportName: "RUR_L1_JUNE_2021.csv",
+    downloadLink: "#",
+  },
+  {
+    date: "02.07.2021",
+    reportName: "RUR_L2_2021_JUNE.csv",
+    downloadLink: "#",
+  },
+  {
+    date: "01.07.2021",
+    reportName: "APP_USAGE_JUNE_2021.csv",
+    downloadLink: "#",
+  },
+  {
+    date: "30.06.2021",
+    reportName: "USER_ACTIVITY_JUNE_2021.csv",
+    downloadLink: "#",
+  },
+  {
+    date: "29.06.2021",
+    reportName: "RUR_L3_06_2021.csv",
+    downloadLink: "#",
+  },
+  {
+    date: "28.06.2021",
+    reportName: "SALES_REPORT_JUNE_2021.csv",
+    downloadLink: "#",
+  },
+  {
+    date: "27.06.2021",
+    reportName: "RUR_L1_JUNE_2021.csv",
+    downloadLink: "#",
+  },
+  {
+    date: "26.06.2021",
+    reportName: "RUR_L2_JUNE_2021.csv",
+    downloadLink: "#",
+  },
+  {
+    date: "25.06.2021",
+    reportName: "APP_USAGE_06_2021.csv",
+    downloadLink: "#",
+  },
+  {
+    date: "24.06.2021",
+    reportName: "USER_STATS_JUNE_2021.csv",
+    downloadLink: "#",
+  },
+  {
+    date: "23.06.2021",
+    reportName: "SALES_REPORT_2021_JUNE.csv",
+    downloadLink: "#",
+  },
+  {
+    date: "22.06.2021",
+    reportName: "RUR_L1_JUNE_2021.csv",
+    downloadLink: "#",
+  },
+  {
+    date: "21.06.2021",
+    reportName: "RUR_L2_2021_JUNE.csv",
+    downloadLink: "#",
+  },
+];
+
+export default reportData;
